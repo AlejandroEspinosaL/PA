390409a9fefa28121f7cf79660f648f93c126d3d
AlejandroEspinosaL <159501196+AlejandroEspinosaL@users.noreply.github.com>
2024-02-22 00:01:35 -0500

    Update Clase_2024_0220.py
    una vez comprobado todo, añadí los Doctest con los resultados arrojados por Python (previamente verificados por mi), tambien borre la definicion de las listas pero fue mas por capricho

--- Clase_2024_0220.py
+++ Clase_2024_0220.py
@@ -1,32 +1,48 @@
-U = [ 1 , 2, 4 ]
-V = [ 4 , 7, 10 ]
-
-
 def dist_l1(U, V):
+    """
+    >>>dist_l1([ 1 , 2, 4 ],[ 4 , 7, 10 ])
+    14
+    """
     dist_tax = 0
     for i in range(len(U)):
         dist_tax += abs(U[i]-V[i])
     return (dist_tax)
 
 def dist_l2(U,V):
+    """
+    >>>dist_l2([ 1 , 2, 4 ],[ 4 , 7, 10 ])
+    8.366600265340756
+    """
     dist_ecul=0
     for i in range (len(U)):
         dist_ecul += ((U[i]-V[i])**2)
     return (dist_ecul**0.5)
 
 def dist_linf(U,V):
+    """
+    >>>dist_linf([ 1 , 2, 4 ],[ 4 , 7, 10 ])
+    6
+    """
     dist_inf=0
     for i in range (len(U)):
         dist_inf = max(dist_inf,abs(U[i]-V[i]))
     return (dist_inf)
 
 def promedio(U):
+    """
+    >>>promedio([ 1 , 2, 4 ])
+    2.3333333333333335
+    """
     miu=0
     for i in range (len(U)):
         miu += U[i]
     return (miu/len(U))
 
 def Desv_est(U):
+    """
+    >>>Desv_est([ 1 , 2, 4 ])
+    1.247219128924647
+    """
     lam=0
     prom= promedio(U)
     for i in range (len(U)):
@@ -34,4 +50,9 @@ def Desv_est(U):
     return ((lam/len(U))**0.5)
 
 def varianza(U):
+    """
+    >>>varianza([ 1 , 2, 4 ])
+    1.5555555555555554
+    """
     return (Desv_est(U) ** 2)
+
