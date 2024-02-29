d56b55bb3da35c752ad3e4ad5423e90429b86fa4
AlejandroEspinosaL <159501196+AlejandroEspinosaL@users.noreply.github.com>
2024-02-29 00:27:17 -0500

    Update Clase_0227_3nplus1.py
    Cambios:
    -añadí los comentarios en el doctest para explicar el programa
    arreglo del código:
    -escribir bien la variable(estaba todo en términos de i y la función estaba en términos de n)
    -arreglos en sintaxis de una función (3n+1 => 3*n+1) 
    -creo que no mas

--- pa/Clase_0227_3nplus1.py
+++ pa/Clase_0227_3nplus1.py
@@ -1,13 +1,22 @@
 def cont_ciclos(n):
-  """
-  cont_ciclos(3)
-  return 7
-  """
-  num_ciclos = 0
-while not (i=1):
-  if (i%=0):
-    i = i/2
-  else:
-    i = 3i+1
-  num_ciclos += 1
-return (num_ciclos)
+    """
+    cont_ciclos(3)
+    return 7
+    
+    #Esta función cuenta la cantidad de ciclos en la función de la conjetura de Collatz un problema matemático
+    #tambien conocido como 3x+1, la idea la siguiente: el numero positivo que entra en la función si es
+    #par se dividira entre 2 y si es impar se multiplicara por 3 y se le suma 1, el numero resultante
+    #se le aplica nuevamente esta funcion y asi hasta que llega a 1(todos llegan eventualmente ahi, de eso trata la conjetura)
+    #esta funcion cuenta cuantos ciclos le toma llegar a 1
+    
+    #ejemplo teorico n=3(impar)n=10(par)n=5(impar)n=16(par)n=8(par)n=4(par)n=2(par)n=1(alto)
+    #numero de ciclos=7
+    """
+    num_ciclos = 0
+    while not (n==1):
+        if (n % 2 ==0):
+            n = n/2
+        else:
+            n = 3*n+1
+        num_ciclos += 1
+    return (num_ciclos)
