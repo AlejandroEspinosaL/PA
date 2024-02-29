79451c229b46d5ba6f619ca7fab45f10a69546c4
AlejandroEspinosaL <159501196+AlejandroEspinosaL@users.noreply.github.com>
2024-02-28 23:48:33 -0500

    Create Clase_0227_3nplus1.py
    código tal cual escrito de la hoja del ejercicio propuesto en clase

---
+++ pa/Clase_0227_3nplus1.py
@@ -0,0 +1,13 @@
+def cont_ciclos(n):
+  """
+  cont_ciclos(3)
+  return 7
+  """
+  num_ciclos = 0
+while not (i=1):
+  if (i%=0):
+    i = i/2
+  else:
+    i = 3i+1
+  num_ciclos += 1
+return (num_ciclos)
